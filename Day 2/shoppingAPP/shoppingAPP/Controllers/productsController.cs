b516fb5b5cf96c56a315e4810fbbce23f5248f91
Nikhil <nikhilshah.net@hotmail.com>
2023-04-27 10:54:26 +0530

    Day 2 MVC

---
+++ Day 2/shoppingAPP/shoppingAPP/Controllers/productsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoppingAPP.Controllers
+{
+    public class productsController : Controller
+    {
+      
+        public IActionResult ProductList()
+        {
+            //collect data from model
+            return View();
+        }
+
+        public IActionResult TopSellingProducts()
+        {
+            return View();
+        }
+
+        public IActionResult Sale()
+        {
+            return View();
+        }
+
+        public IActionResult AppSummary()
+        {
+            ViewBag.totalProducts = 10;
+            ViewBag.developer = "Nikhil Shah";
+            ViewBag.isAppLive = true;
+            ViewBag.projectCost = 2.5;
+
+            List<string> teamMembers = new List<string>()
+            {
+                "Nikhil", "Akshay","Rahul","Rohan","Roshan","Mohan","Minaxi"
+            };
+
+            ViewBag.team = teamMembers;
+
+            return View();
+        }
+
+    }
+}
