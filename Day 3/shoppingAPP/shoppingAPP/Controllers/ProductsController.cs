d2ff86daffa30a5bca118c5be96ac5341821feec
Nikhil <nikhilshah.net@hotmail.com>
2023-04-28 10:55:52 +0530

    Day 3 - MVC

---
+++ Day 3/shoppingAPP/shoppingAPP/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPP.Models;
+namespace shoppingAPP.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        //this is not a good code, we will use DI here
+        Products pObj = new Products(); 
+
+        public IActionResult ProductList()
+        {
+            ViewBag.pCategories = pObj.GetProductCategories();
+            ViewBag.pList = pObj.GetAllProducts();
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ Day 3/shoppingAPP/shoppingAPP/Models/Products.cs
@@ -0,0 +1,37 @@
+﻿namespace shoppingAPP.Models
+{
+    public class Products
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+      static  List<string> productCategories = new List<string>()
+        {
+            "Electronics","Shoes","Tshirts","Watch","Cold-Drinks","Accessories","Books"
+        };
+
+        public List<string> GetProductCategories()
+        {
+            return productCategories;
+        }
+        //if not static - this is going into endless loop - product inside product and product product 
+       static  private List<Products> pList = new List<Products>()
+        {
+            new Products() { pId = 1, pName ="Iphone", pCategory="Electronics", pIsInStock=true, pPrice=140000},
+            new Products() { pId = 2, pName ="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new Products() { pId = 3, pName ="Appy", pCategory="Cold-Drink", pIsInStock=false, pPrice=90},
+            new Products() { pId = 4, pName ="Dell Lattitude", pCategory="Electronics", pIsInStock=true, pPrice=80000},
+            new Products() { pId = 5, pName ="Fossil Q", pCategory="Watch", pIsInStock=false, pPrice=35000},
+            new Products() { pId = 6, pName ="Apple Watch", pCategory="Watch", pIsInStock=true, pPrice=60000},
+            new Products() { pId = 7, pName ="Nike Air", pCategory="Shoes", pIsInStock=true, pPrice=12000},
+        };
+
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+    }
+}
