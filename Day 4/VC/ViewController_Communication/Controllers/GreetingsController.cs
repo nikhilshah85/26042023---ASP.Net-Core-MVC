ff0f118fce725ca0215fe86927f27f05e29db2dd
Nikhil <nikhilshah.net@hotmail.com>
2023-05-02 10:57:13 +0530

    Day 4 - View to Controller

---
+++ Day 4/VC/ViewController_Communication/Controllers/GreetingsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewController_Communication.Controllers
+{
+    public class GreetingsController : Controller
+    {
+      
+        public IActionResult Greet()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Greet(string guestName)
+        {
+            string message = "";
+            bool hasError = false;
+
+            if (guestName == "" || guestName == null)
+            {
+                hasError = true;
+                message = "Name cannot be left blank";
+            }
+            else if (guestName.Length < 3)
+            {
+                hasError = true;
+                message = "Please enter a valid Name";
+             
+            }
+          
+            else
+            {
+                message  = "Hello and welcome to the development world of MVC - " + guestName + " create better web apps";
+            }
+            ViewBag.hasError = hasError;
+            ViewBag.greetMessage = message;
+            return View();
+        }
+
+
+    }
+}
---
+++ Day 4/VC/ViewController_Communication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewController_Communication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
