bcac010801e212c033dc4a2a0a8d2ebe3532855d
Nikhil <nikhilshah.net@hotmail.com>
2023-05-03 11:00:00 +0530

    Day 5

---
+++ Day 5/employeeManagementAPP/employeeManagementAPP/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using employeeManagementAPP.Models;
+namespace employeeManagementAPP.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+
+
+        EmployeeModel eObj = new EmployeeModel(); //this is bad, we will use DI instead
+
+
+
+        public IActionResult EmployeeList()
+        {
+            ViewBag.eList = eObj.GetAllEmployees();
+            return View();
+        }
+    }
+}
---
+++ Day 5/employeeManagementAPP/employeeManagementAPP/Models/EmployeeModel.cs
@@ -0,0 +1,36 @@
+﻿namespace employeeManagementAPP.Models
+{
+    public class EmployeeModel
+    {
+        public int empNo { get; set; }
+        public string empName { get; set; }
+        public string empDesignation { get; set; }
+        public double empSalary { get; set; }
+        public bool empIsPermenant { get; set; }
+
+        private static List<EmployeeModel> eList = new List<EmployeeModel>()
+        {
+            new EmployeeModel(){ empNo=101, empName="Nikhil", empSalary=12000, empDesignation="Consultant", empIsPermenant=true  },
+            new EmployeeModel(){ empNo=102, empName="Sameer", empSalary=23000, empDesignation="HR", empIsPermenant=true  },
+            new EmployeeModel(){ empNo=103, empName="Pratik", empSalary=21000, empDesignation="Consultant", empIsPermenant=false  },
+            new EmployeeModel(){ empNo=104, empName="Keshav", empSalary=42000, empDesignation="Consultant", empIsPermenant=true  },
+            new EmployeeModel(){ empNo=105, empName="Mohit", empSalary=8000, empDesignation="Sales", empIsPermenant=false  },
+        };
+
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            return eList;
+        }
+
+        public EmployeeModel GetEmployeeById(int eid)
+        {
+            var emp = eList.Find(e => e.empNo == eid);
+
+            if (emp != null)
+            {
+                return emp;
+            }
+            throw new Exception("Employee  not  found in system");
+        }
+    }
+}
