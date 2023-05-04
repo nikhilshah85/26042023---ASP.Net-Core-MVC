7a6c374c1bbefd9f4ff821cd790c4c0c92e59a8d
Nikhil <nikhilshah.net@hotmail.com>
2023-05-04 10:57:19 +0530

    Day 6

---
+++ Day 6/bankingSystemMVC/bankingSystemMVC/Controllers/AccountsController.cs
@@ -0,0 +1,52 @@
+﻿using bankingSystemMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using bankingSystemMVC.Models;
+namespace bankingSystemMVC.Controllers
+{
+    public class AccountsController : Controller
+    {
+        Accounts accObj = new Accounts();
+        public IActionResult DisplayAccountList()
+        {
+            ViewBag.accList = accObj.GetAccounts();
+            return View();
+        }
+        //create a view based on it and display the data in table
+
+        public IActionResult GetAccountById()
+        {
+            ViewBag.errMessage = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetAccountById(int id)
+        {
+            try
+            {
+                var acc = accObj.GetAccountById(id);
+                ViewBag.acc = acc;
+                return View();
+            }
+            catch (Exception es)
+            {
+                ViewBag.errMessage = es.Message;
+                return View();
+            }
+        }
+
+        public IActionResult AccNewAccount()
+        {
+            ViewBag.addResult = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AccNewAccount(Accounts newacc)
+        {
+            ViewBag.addResult =  accObj.AddNewAccount(newacc);
+            return View();
+        }
+
+    }
+}
---
+++ Day 6/bankingSystemMVC/bankingSystemMVC/Models/Accounts.cs
@@ -0,0 +1,60 @@
+﻿namespace bankingSystemMVC.Models
+{
+    public class Accounts
+    {
+        public int accNo { get; set; }
+        public string accName { get; set; }
+        public double accBalance { get; set; }
+        public string accType { get; set; }
+        public bool accIsActive { get; set; }
+
+        static List<Accounts> accList = new List<Accounts>()
+        {
+            new Accounts(){ accNo=101, accName="Nikhil", accBalance=5000, accIsActive=true, accType="Savings"},
+            new Accounts(){ accNo=102, accName="Sakshi", accBalance=6000, accIsActive=true, accType="Savings"},
+            new Accounts(){ accNo=103, accName="Kriti", accBalance=7000, accIsActive=true, accType="Current"},
+            new Accounts(){ accNo=104, accName="Mohit", accBalance=8000, accIsActive=false, accType="Savings"},
+            new Accounts(){ accNo=105, accName="Suman", accBalance=9000, accIsActive=true, accType="Savings"},
+            new Accounts(){ accNo=106, accName="Rohit", accBalance=1000, accIsActive=false, accType="Current"}
+        };
+
+        public List<Accounts> GetAccounts()
+        {
+            return accList;
+        }
+
+        public Accounts GetAccountById(int id)
+        {
+            var acc = accList.Find(a => a.accNo == id);
+            if (acc != null)
+            {
+                return acc;
+            }
+            throw new Exception("Account Not found");
+        }
+
+        public string AddNewAccount(Accounts newAccount)
+        {
+            accList.Add(newAccount);
+            return "Account Added Successfully";
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
